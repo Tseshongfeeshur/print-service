8a28205d9dcffd475fe4050b3fd06a5005496ee6
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-03-19 14:00:02 +0800

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -150,4 +150,4 @@ def get_subfiles():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=632, debug=True)
+    app.run(host='0.0.0.0', port=632, debug=False)
